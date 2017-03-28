2a5ccbbe8f90e666522d9c9ad63f66cbdd1fe737
t_jeanneau <hi@thomasjeanneau.com>
2017-03-28 15:36:34 +0200

    update env

--- bot/pairingConversation.js
+++ bot/pairingConversation.js
@@ -2,7 +2,8 @@
  * Created by thomasjeanneau on 20/03/2017.
  */
 
-import slack from 'slack';
+import pick from 'lodash/pick';
+import find from 'lodash/find';
 
 import { controller, base } from './configSlackbot';
 import { checkIfAdmin } from './methods';
@@ -12,19 +13,31 @@ controller.hears("introductions", "direct_message", function (bot, message) {
     .then((res) => {
     if (res) {
       bot.reply(message, "Ok, I'll start introducing people");
-      base('Pairings').select({
-        view: "Main View"
-      }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-          slack.api.channels.create({token, name}, (err, data) => { })
-          console.log('Retrieved', record.get('Id'));
-        });
-        fetchNextPage();
-      }, function done(err) {
+      bot.api.users.list({}, (err, { members }) => {
         if (err) {
           console.error(err);
+          return;
         }
-
+        const list = pick(members, ['id', 'name']);
+        base('Pairings').select({
+          view: "Main View"
+        }).eachPage(function page(records, fetchNextPage) {
+          records.forEach(function(record) {
+            const users = [];
+            users.push(find(list, ['name', record.get('Teacher')]));
+            users.push(find(list, ['name', record.get('Learner')]));
+            users.push(find(list, ['name', 'pairingbot']));
+            bot.api.usergroups.create({name: 'new paring !', users}, (err, data) => {
+              console.log(data);
+            });
+            console.log('Retrieved', record.get('Id'));
+          });
+          fetchNextPage();
+        }, function done(err) {
+          if (err) {
+            console.error(err);
+          }
+        });
       });
     } else {
       bot.reply(message, "Sorry but it looks like you're not an admin. You can't use this feature.")
