a2203827a243860693b79af63b16767b32e555e7
Davide Bonapersona <davide.bonapersona@gmail.com>
2017-05-10 18:43:58 -0400

    added saving and deleting a pairing; uses Promises

--- airtable/index.js
+++ airtable/index.js
@@ -1,3 +1,4 @@
+import Promise from 'bluebird'
 import _ from 'lodash'
 import Airtable from 'airtable'
 import settings from '../bot/settings'
@@ -18,13 +19,15 @@ export const base = Airtable.base(AIRTABLE_BASE_KEY)
 
 // reads all records from a table
 const _getAllRecords = (select, callback) => {
-  let allRecords = []
-  select.eachPage((records, fetchNextPage) => {
-    allRecords = allRecords.concat(records)
-    fetchNextPage()
-  }, (err) => {
-    if (err) return callback(err)
-    callback(undefined, allRecords)
+  return new Promise((resolve, reject) => {
+    let allRecords = []
+    select.eachPage((records, fetchNextPage) => {
+      allRecords = allRecords.concat(records)
+      fetchNextPage()
+    }, (err) => {
+      if (err) return reject(err)
+      resolve(allRecords)
+    })
   })
 }
 
@@ -32,93 +35,106 @@ const _getAllRecords = (select, callback) => {
 // reads all people from Airtable, and returns them
 // as an Array of
 //  {name: String, interests: [String], skills: [String]}
-export const getAllPeople = (applicantsTableName, membersTableName, callback) => {
+export const getAllPeople = (applicantsTableName, membersTableName) => {
   if (applicantsTableName === 'P2PL Tests') {
-    // TODO: this is for backwards compatibility with the old
-    // P2PL Tests table - remove this when we stop using it
-    _getAllRecords(base(tableName).select({
+    // TODO: this is temporary code to handle the P2PL Tests table
+    // and support backwards compatibility
+    // Should be removed when we stop using that table
+    return _getAllRecords(base(applicantsTableName).select({
       view: "Main View",
-      fields: ["Name", "Interests", "Skills", "Slack Handle", "Pairing admin"]
-    }), (err, applicantsRecords) => {
-      if (err) return callback(err)
+      fields: ["Slack Handle", "Interests", "Skills", "Pairing admin"]
+    })).then((applicants) => {
       const people = []
-      applicantsRecords.forEach((r) => {
+      applicants.forEach((r) => {
         const interests = r.get('Interests'),
               skills = r.get('Skills'),
-              name = r.get('Name'),
-              slack_handle = r.get('Slack Handle'),
-              is_admin = !!r.get('Pairing admin')
+              name = r.get('Slack Handle'),
+              isAdmin = !!r.get('Pairing admin')
         // require name, default skills and interests to []
-        if (name && name.length && slack_handle && slack_handle.length) {
+        if (name && name.length) {
           people.push({
             name,
             interests: (interests || []),
             skills: (skills || []),
-            slackHandle,
             isAdmin,
           })
         }
       })
-      callback(undefined, people)
-    })
-  } else {
-    // this is based on the structure of the P2PL Applicants table,
-    // which references records from the Members table
-    const people = []
-    const F_MEMBER = 'Applicant',
-          F_INTERESTS = 'Interests',
-          F_SKILLS = 'Skills',
-          F_ADMIN = 'Admin',
-          F_NAME = 'Name',
-          F_SLACK = 'Slack Handle'
-    _getAllRecords(base(applicantsTableName).select({
-      view: "Main View",
-      fields: [F_MEMBER, F_INTERESTS, F_SKILLS, F_ADMIN],
-    }), (err, peopleRecords) => {
-      if (err) return callback(err)
-      let people = _.reduce(peopleRecords, (all, r) => {
-        const memberId = (r.get(F_MEMBER) || [])[0],
-              interests = (r.get(F_INTERESTS) || []),
-              skills = (r.get(F_SKILLS) || []),
-              isAdmin = !!r.get(F_ADMIN)
-        // require memberId to be set
-        if (memberId && memberId.length) {
-          all.push({memberId, interests, skills, isAdmin})
-        }
-        return all
-      }, [])
-      // fetch corresponding Members
-      _getAllRecords(base(membersTableName).select({
-        view: "Main View",
-        fields: [F_NAME, F_SLACK],
-        // TODO: can we retrieve just records with our memberIds?
-      }), (err, memberRecords) => {
-        if (err) return callback(err)
-        // build a map of memberId => {name, slackHandle}
-        const membersById = _.reduce(memberRecords, (all, r) => {
-          return _.set(all, r.getId(), {
-            name: r.get(F_NAME),
-            slackHandle: r.get(F_SLACK),
-          })
-        }, {})
-        // merge all people with the member data, ignoring
-        // people without a slack handle
-        people = _.reduce(people, (all, person) => {
-          const member = membersById[person.memberId]
-          if (member && member.slackHandle) {
-            all.push({
-              slackHandle: member.slackHandle.replace(/^@/, ''),
-              name: member.name,
-              skills: person.skills,
-              interests: person.interests,
-              isAdmin: person.isAdmin,
-            })
-          }
-          return all
-        }, [])
-        // return people
-        callback(undefined, people)
-      })
+      return people
     })
   }
+  // Code below handles the structure of the new P2PL Applicants table
+  return _getAllRecords(base(applicantsTableName).select({
+    view: 'Main View',
+    fields: ['Slack Handle', 'Interests', 'Skills', 'Admin']
+  })).then((records) => {
+    return _.reduce(records, (people, r) => {
+      const name = (r.get('Slack Handle') || [])[0]
+      if (name && name.length) {
+        people.push({
+          name: name.replace(/^@/, ''),
+          interests: (r.get('Interests') || []),
+          skills: (r.get('Skills') || []),
+          isAdmin: !!r.get('Admin'),
+        })
+      }
+      return people
+    }, [])
+  })
+}
+
+
+// reads a Pairing from Airtable
+export const getPairing = (tableName, pairingId) => {
+  return _getAllRecords(base(tableName).select({
+    view: "Main View",
+    fields: ['Teacher', 'Learner', 'Skill'],
+    filterByFormula: `{Pairing Id}='${pairingId}'`,
+  })).then((pairingRecords) => {
+    return {
+      id: pairingId,
+      pairs: _.map(pairingRecords, (r) => {
+        return {
+          teacherName: r.get('Teacher'),
+          learnerName: r.get('Learner'),
+          skills: r.get('Skill'),
+        }
+      })
+    }
+  })
+}
+
+
+// saves a Pairing to Airtable
+export const savePairing = (tableName, pairing) => {
+  // ensure we have the proper structure
+  if (!pairing.id) return reject("missing pairing.id")
+  if (!_.isArray(pairing.pairs)) return reject("invalid pairing.pairs")
+  // write the pairs to Airtable
+  const create = Promise.promisify(base(tableName).create)
+  return Promise.map(pairing.pairs, (pair) => {
+    return create({
+      'Pairing Id': pairing.id,
+      'Teacher': pair.teacherName,
+      'Learner': pair.learnerName,
+      'Skill': pair.skills,
+    })
+  }).then((results) => {
+    return pairing
+  })
+}
+
+
+// removes a Pairing from Airtable
+export const destroyPairing = (tableName, pairingId) => {
+  return _getAllRecords(base(tableName).select({
+    view: "Main View",
+    fields: [],
+    filterByFormula: `{Pairing Id}='${pairingId}'`,
+  })).then((pairingRecords) => {
+    const destroy = Promise.promisify(base(tableName).destroy)
+    return Promise.map(pairingRecords, (record) => {
+      return destroy(record.getId())
+    })
+  }).then(() => pairingId)
 }
--- test/airtable_test.js
+++ test/airtable_test.js
@@ -1,6 +1,10 @@
 import _ from 'lodash'
 import {expect} from './helper'
-import {getAllPeople, getPairing} from '../airtable'
+import {getAllPeople, getPairing, savePairing, destroyPairing} from '../airtable'
+
+
+const PAIRINGS_TABLE = 'Pairings'
+const SKILLS = ['Node.js', 'UX Design']
 
 
 describe("airtable", function() {
@@ -8,13 +12,14 @@ describe("airtable", function() {
   describe(".getAllPeople", function() {
 
     function expectSuccessful(applicantsTableName, membersTableName) {
-      it("retrieves people, skills and interests", function(done) {
-        getAllPeople(applicantsTableName, membersTableName, function(err, people) {
-          expect(err).to.be.undefined
+      it("retrieves people, skills and interests", function() {
+        return getAllPeople(applicantsTableName, membersTableName)
+        .then((people) => {
           expect(people).to.be.an('array')
           expect(people).not.to.be.empty
           people.forEach(function(person) {
             expect(person).to.be.an('object')
+            expect(_.keys(person)).to.have.members(['name', 'interests', 'skills', 'isAdmin'])
             expect(person.name).to.be.a('string')
             expect(person.name).not.to.be.empty
             expect(person.interests).to.be.a('array')
@@ -25,12 +30,8 @@ describe("airtable", function() {
             person.skills.forEach(function(i) {
               expect(i).to.be.a('string')
             })
-            expect(person.slackHandle).to.be.a('string')
-            expect(person.slackHandle).not.to.be.empty
-            expect(person.slackHandle[0]).not.to.eq('@')
             expect(person.isAdmin).to.be.a('boolean')
           })
-          done()
         })
       })
     }
@@ -41,6 +42,59 @@ describe("airtable", function() {
 
     describe("with the new 'P2PL Applicants' table", function() {
       expectSuccessful('P2PL Applicants', 'Members')
+    })
+  })
+
+  describe(".getPairing", function() {
+    const pairingId = "201704170001"
+
+    it ("returns an existing pairing", function() {
+      return getPairing(PAIRINGS_TABLE, pairingId).then((pairing) => {
+        expect(pairing).to.be.an('object')
+        expect(pairing.id).to.eq(pairingId)
+        expect(pairing.pairs).to.be.an('array')
+        _.each(pairing.pairs, (pair) => {
+          expect(pair).to.be.an('object')
+          expect(pair.teacherName).to.be.a('string')
+          expect(pair.learnerName).to.be.a('string')
+          expect(pair.skills).to.be.an('array')
+          expect(pair.skills).not.to.be.empty
+        })
+      })
+    })
+  })
+
+  describe(".savePairing and .destroyPairing", function() {
+    const pairingId = `test_${Date.now().toString()}`
+
+    it ("saves, reads, and destroys a pairing", function() {
+      const pairing = {
+        id: pairingId,
+        pairs: [
+          {teacherName: 'test1', learnerName: 'test2',
+            skills: _.sampleSize(SKILLS, 2)},
+        ]
+      }
+      return savePairing(PAIRINGS_TABLE, pairing).then((pairing) => {
+        expect(pairing.id).to.eq(pairingId)
+        // try to read back the pairing
+        return getPairing(PAIRINGS_TABLE, pairingId).then((readPairing) => {
+          // assert the pairing was properly created
+          expect(readPairing.id).to.eq(pairingId)
+          expect(readPairing.pairs.length).to.eq(pairing.pairs.length)
+          _.each(readPairing.pairs, (readPair, i) => {
+            const pair = pairing.pairs[i]
+            expect(readPair.teacherName).to.eq(pair.teacherName)
+            expect(readPair.learnerName).to.eq(pair.learnerName)
+          })
+        }).then(() => {
+          // try to destroy the pairing
+          return destroyPairing(PAIRINGS_TABLE, pairingId)
+            .then((destroyedPairingId) => {
+              expect(destroyedPairingId).to.eq(pairingId)
+            })
+        })
+      })
     })
   })
 
