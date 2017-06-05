73488447445e4bd490d99f436e783f298145f6c6
t_jeanneau <hi@thomasjeanneau.com>
2017-06-05 20:13:49 +0200

    add mongodb

---
+++ main/mongodb/index.js
@@ -0,0 +1,54 @@
+/**
+ * Created by thomasjeanneau on 05/06/2017.
+ */
+
+import Promise from 'bluebird'
+import MongoClient from 'mongodb'
+
+require('dotenv').config()
+
+const {MONGO_URL} = process.env
+
+export const find = async (name, selector) => {
+  const connect = Promise.promisify(MongoClient.connect)
+  const db = await connect(MONGO_URL)
+  const collection = db.collection(name)
+  const findOne = Promise.promisify(collection.findOne)
+  const result = await findOne(selector)
+  console.log(`find a document with selector ${JSON.stringify(selector)} in ${name} collection: ${JSON.stringify(result)}`)
+  db.close()
+  return result
+}
+
+export const insert = async (name, obj) => {
+  const connect = Promise.promisify(MongoClient.connect)
+  const db = await connect(MONGO_URL)
+  const collection = db.collection(name)
+  const insertOne = Promise.promisify(collection.insertOne)
+  const result = await insertOne(obj)
+  console.log(`insert a document with object ${JSON.stringify(obj)} in ${name} collection: ${JSON.stringify(result)}`)
+  db.close()
+  return result
+}
+
+export const update = async (name, selector, obj) => {
+  const connect = Promise.promisify(MongoClient.connect)
+  const db = await connect(MONGO_URL)
+  const collection = db.collection(name)
+  const updateOne = Promise.promisify(collection.updateOne)
+  const result = await updateOne(selector, obj)
+  console.log(`update a document with selector ${JSON.stringify(selector)} and object ${JSON.stringify(obj)} in ${name} collection: ${JSON.stringify(result)}`)
+  db.close()
+  return result
+}
+
+export const remove = async (name, selector) => {
+  const connect = Promise.promisify(MongoClient.connect)
+  const db = await connect(MONGO_URL)
+  const collection = db.collection(name)
+  const deleteOne = Promise.promisify(collection.deleteOne)
+  const result = await deleteOne(selector)
+  console.log(`remove a document with selector ${JSON.stringify(selector)} in ${name} collection: ${JSON.stringify(result)}`)
+  db.close()
+  return result
+}
