82854891a488c5e7171cc6fc4ee749ffefab42d3
Jonathan Pepin <jonathanp@uber.com>
2017-06-25 11:25:52 +0200

    draft on interaction

--- main/bot/hears.js
+++ main/bot/hears.js
@@ -24,9 +24,9 @@ if (!NODE_ENV) {
 
 // Helper Methods
 const giveHelp = function() {
-  return `"!newloc <city or country>" for me to update your location,
-  "!myloc" if you want to know where I think you are,
-  "!map" for a link to Mangrove Members map!`
+  return `'!newloc <city or country>' for me to update your location,
+  '!myloc' if you want to know where I think you are,
+  '!map' for a link to Mangrove Members map!`
 };
 
 export const handleError = function(e, bot) {
@@ -35,7 +35,6 @@ export const handleError = function(e, bot) {
 }
 
 // User Commands
-
 controller.hears(['^Hello', '^Yo', '^Hey', '^Hi', '^Ouch'], ['direct_message', 'direct_mention'], async (bot, message) => {
   try {
     const {name} = await getSlackUser(bot, message.user)
@@ -45,6 +44,23 @@ controller.hears(['^Hello', '^Yo', '^Hey', '^Hi', '^Ouch'], ['direct_message', '
       convo.say(`I'm Marco Polo! I'm taking care of keeping everyone's location up to date :boat:`)
       convo.say(`Here are the few commands you can use with me :ok_woman:`)
       convo.say(giveHelp())
+    })
+  } catch (e) {
+    handleError(e, bot)
+  }
+})
+
+controller.hears(['^help', '^options$'], ['direct_message', 'direct_mention'], async (bot, message) => {
+  try {
+    const {name} = await getSlackUser(bot, message.user)
+    const botReply = Promise.promisify(bot.reply)
+    await botReply(message, `Hi ${name}! What can I do for you ? :slightly_smiling_face:`)
+    await botReply(message, {
+      attachments: [{
+        pretext: 'This is what you can ask me:',
+        text: giveHelp(),
+        mrkdwn_in: ['text', 'pretext']
+      }]
     })
   } catch (e) {
     handleError(e, bot)
@@ -86,33 +102,51 @@ controller.hears(['^!newloc'], ['direct_message', 'direct_mention'], async (bot,
       return
     }
 
-    // TODO: check if multiple addresses have been returned, asked the user to choose the correct one.
     const validatedLocs = await validateAddress(address)
-
-    await botReply(message, {
-      attachments: [{
-        callback_id: '1',
-        attachment_type: 'default',
-        pretext: `I found a matching address, is it correct?`,
-        title: validatedLocs[0].formatted_address,
-        actions: [
-          {
-            "name": "addressConfirmed",
-            "text": "Yes",
-            "type": "button",
-            "value": true,
-            "style": "primary"
-          },
-          {
-            "name": "addressConfirmed",
-            "text": "No",
-            "type": "button",
-            "value": false,
-            "style": "danger"
-          }
-        ]
-      }]
-    })
+    if (validatedLocs.length === 1) {
+      await botReply(message, {
+        attachments: [{
+          callback_id: '1',
+          attachment_type: 'default',
+          pretext: `I found a matching address, is it correct?`,
+          title: validatedLocs[0].formatted_address,
+          actions: [
+            {
+              'name': 'addressConfirmed',
+              'text': 'Yes',
+              'type': 'button',
+              'value': validatedLocs[0].formatted_address,
+              'style': 'primary'
+            },
+            {
+              'name': 'addressConfirmed',
+              'text': 'No',
+              'type': 'button',
+              'value': false,
+              'style': 'danger'
+            }
+          ]
+        }]
+      })
+    } else {
+      const actions = [];
+      for (loc of validatedLocs) {
+        actions.push({
+          name: 'addressSelect',
+          text: loc.formatted_address,
+          'type': 'button',
+          'value': loc.formatted_address
+        })
+      }
+      await botReply(message, {
+        attachments: [{
+          callback_id: '2',
+          attachment_type: 'default',
+          pretext: `I found multiple matching addresses, please select the correct one!`,
+          actions: actions
+        }]
+      })
+    }
 
     // TODO: ask user to confirm they want to change their location to validated address
     // const updatedMember = await updateMember(user.id, {
@@ -126,33 +160,6 @@ controller.hears(['^!newloc'], ['direct_message', 'direct_mention'], async (bot,
     //     mrkdwn_in: ['text', 'pretext']
     //   }]
     // })
-  } catch (e) {
-    handleError(e, bot)
-  }
-})
-
-controller.hears(['^help', '^options$'], ['direct_message', 'direct_mention'], async (bot, message) => {
-  try {
-    const {name} = await getSlackUser(bot, message.user)
-    const botReply = Promise.promisify(bot.reply)
-    await botReply(message, `Hi ${name}! What can I do for you ? :slightly_smiling_face:`)
-    await botReply(message, {
-      attachments: [{
-        pretext: 'This is what you can ask me:',
-        text: giveHelp(),
-        mrkdwn_in: ['text', 'pretext']
-      }]
-    })
-    // const isBuilder = await checkIfBuilder(bot, message)
-    // if (isBuilder) {
-    //   await botReply(message, {
-    //     attachments: [{
-    //       pretext: 'And because you\'re a Builder, you can also do:',
-    //       text: ``,
-    //       mrkdwn_in: ['text', 'pretext']
-    //     }]
-    //   })
-    // }
   } catch (e) {
     handleError(e, bot)
   }
@@ -175,14 +182,28 @@ controller.hears('[^\n]+', ['direct_message', 'direct_mention'], async (bot, mes
 controller.on('interactive_message_callback', function(bot, message) {
   console.log(message)
   switch (message.callback_id) {
+    // address confirmation
     case '1':
       handleAddressConfirmation(bot, message)
+      break
+    // address selection
+    case '2':
+      handleAddressSelect(bot, message)
       break
   }
 })
 
-const handleAddressConfirmation = async function(bot, message) {
-  console.log("in callback 1")
+const handleAddressConfirmation = function(bot, message) {
+  let text;
+  if (message.actions[0].value) {
+    updateMember(message.user.id, {
+    'Postal Adress': message.actions[0].value
+    })
+    text = `:white_check_mark: ${message.actions[0].value}`
+  } else {
+    text = ':x:'
+  }
+
   bot.replyInteractive(message, {
     attachments: [{
       callback_id: '1',
@@ -191,9 +212,27 @@ const handleAddressConfirmation = async function(bot, message) {
       title: validatedLocs[0].formatted_address,
       actions: [
         {
-          "text": ":white_check_mark:"
+          'text': text
         }
       ]
     }]
-});
+  })
+}
+
+const handleAddressSelect = function(bot, message) {
+  updateMember(message.user.id, {
+    'Postal Adress': message.actions[0].value
+  })
+  bot.replyInteractive(message, {
+    attachments: [{
+      callback_id: '2',
+      attachment_type: 'default',
+      pretext: `I found multiple matching addresses, please select the correct one!`,
+      actions: [
+        {
+          'text': `:white_check_mark:`
+        }
+      ]
+    }]
+  })
 }
