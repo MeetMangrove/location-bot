614b7a1c74de5ccdb2b1b36571dd34180813a9fb
Jonathan Pepin <jonathanp@uber.com>
2017-06-25 17:39:55 +0200

    limit to 5 first addresses

--- main/bot/hears.js
+++ main/bot/hears.js
@@ -130,7 +130,7 @@ controller.hears(['^!newloc'], ['direct_message', 'direct_mention'], async (bot,
       })
     } else {
       const actions = [];
-      for (let loc of validatedLocs) {
+      for (let loc of validatedLocs.slice(0, 5)) {
         console.log(validatedLocs)
         let postalCode;
         for (let comp of loc.address_components) {
