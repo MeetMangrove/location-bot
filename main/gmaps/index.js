b0fd77f6926e23d5971df36996bdbeebcd8d36f1
Jonathan Pepin <jypepin@gmail.com>
2017-07-01 16:58:04 +0200

    Conversation flow and cleanup
    * progress on message and flow clean up, first test
    
    * few quick fixes
    
    * handle when None is selected with multiple addresses
    
    * lint and fixes
    
    * use conversation for followup messages
    
    * async for mapmessage
    
    * use convo next to send next msg
    
    * test test
    
    * slowly trying
    
    * finally with mpamessage
    
    * fix async await
    
    * clean up, extract messages in their own file, bla bla bla
    
    * fix bug with getAllMembers

--- main/gmaps/index.js
+++ main/gmaps/index.js
@@ -6,9 +6,21 @@ const gmaps = Gmaps.createClient({
 
 export const validateAddress = async (address) => {
   return new Promise((resolve, reject) => {
-    gmaps.geocode({address}, function(err, response) {
+    gmaps.geocode({address}, function (err, response) {
       if (err) return reject(err)
       resolve(response.json.results)
     })
   })
+}
+
+export const getCityCountry = (gmapsResult) => {
+  let city, country
+  for (const comp of gmapsResult.address_components) {
+    if (comp.types.indexOf('locality') > -1) {
+      city = comp.long_name
+    } else if (comp.types.indexOf('country') > -1) {
+      country = comp.long_name
+    }
+  }
+  return city ? `${city}, ${country}` : country
 }
