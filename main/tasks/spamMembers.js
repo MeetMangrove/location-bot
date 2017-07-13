abb18b22340b5ad22714cca31804096d5f8439ac
Jonathan Pepin <jonathanp@uber.com>
2017-07-13 11:07:18 +0200

    test script

---
+++ main/tasks/spamMembers.js
@@ -0,0 +1,12 @@
+import {bot} from '../bot/config'
+
+const sendPrivateMessage = function(slackId, message) {
+  bot.startPrivateConversation({user: slackId}, (err, convo) => {
+    if (err) return console.error(err)
+    console.log('in function')
+    convo.say(message)
+  })
+}
+
+console.log("sending message to Jonathan")
+sendPrivateMessage('U30MX249Z', 'hello Jon!');
