af9ebef4bc1ccada8d2b91e0784634949adf29d5
Jonathan Pepin <jonathanp@uber.com>
2017-07-13 11:25:32 +0200

    with promise

--- main/tasks/spamMembers.js
+++ main/tasks/spamMembers.js
@@ -1,14 +1,15 @@
 import {bot} from '../bot/config'
 
 const sendPrivateMessage = function(slackId, message) {
-  bot.startPrivateConversation({user: slackId}, (err, convo) => {
-    if (err) return console.error(err)
-    console.log('in function')
-    convo.say(message)
-    process.exit(0);
+  return new Promise((resolve, reject) => {
+    bot.startPrivateConversation({user: slackId}, (err, convo) => {
+      if (err) return console.error(err)
+      console.log('in function')
+      convo.say(message)
+    })
   })
 }
 
 console.log("sending message to Jonathan")
-sendPrivateMessage('U30MX249Z', 'hello Jon!');
+sendPrivateMessage('U30MX249Z', 'hello Jon!').then(process.exit(0));
 
